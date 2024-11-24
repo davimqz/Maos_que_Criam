27c19af219d1b60dc1e0cf552625e6314b413a10
Lucas Calabria <lvcalabria@gmail.com>
2024-11-24 16:06:59 -0300

    base do teste de enviar perguntas

---
+++ cypress/e2e/enviarper.cy.js
@@ -0,0 +1,9 @@
+describe('enviar pergunta', () => {
+    it('teste', () => {
+  
+      cy.visit('http://127.0.0.1:8000/enviar-pergunta/')
+      cy.get('textarea').type('eu posso enviar quantos produtos por vez ?')
+      cy.get('button').click()
+    })
+    
+  })
